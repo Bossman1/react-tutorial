0c6ec010049e44bd9e20855beb24c5dbc085b4df
Nick work PC <nick@netcraft.ge>
2023-05-12 14:14:07 +0400

    init

---
+++ react/src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import {BeakerIcon, UserIcon} from "@heroicons/react/24/solid";
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-purple-500">
+React.js
+
+          <BeakerIcon className="w-4" />
+          <UserIcon className="w-4" />
+      </div>
+    </>
+  )
+}
+
+export default App
